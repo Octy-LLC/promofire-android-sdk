6b629faa45658a814fdfb308c12fdf5d1d01abff
Konstantin <kostyak13k@gmail.com>
2024-11-27 10:53:54 +0200

    Init commit

---
+++ promofire/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+apply { from(rootProject.file("detekt/config.gradle")) }
+
+android {
+    namespace = "io.promofire"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_21.majorVersion
+    }
+}
+
+dependencies {
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization)
+
+    // Ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+
+    // Test
+    testImplementation(libs.junit)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+rootProject.name = "promofire-android-sdk"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":promofire")
