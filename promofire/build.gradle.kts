1c30f6a083b6f9c6d4050fdc38d62ea72b03a3f3
Konstantin <kostyak13k@gmail.com>
2024-12-02 23:01:10 +0200

    Add dependencies
    - ktor-client-auth
    - ktor-client-logging
    - ktor-client-content-negotiation
    - ktor-serialization-kotlinx

--- promofire/build.gradle.kts
+++ promofire/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     // Ktor
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.auth)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx)
 
     // Test
     testImplementation(libs.junit)
