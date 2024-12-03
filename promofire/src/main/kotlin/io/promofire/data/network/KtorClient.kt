be062aabda4c1908d7a1435707246b2d171281e4
Konstantin <kostyak13k@gmail.com>
2024-12-03 22:09:51 +0200

    Add logger to ktor client

--- promofire/src/main/kotlin/io/promofire/data/network/KtorClient.kt
+++ promofire/src/main/kotlin/io/promofire/data/network/KtorClient.kt
@@ -4,8 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.headers
 import io.ktor.serialization.kotlinx.json.json
+import io.promofire.logger.Logger
+import io.promofire.logger.i
+import io.ktor.client.plugins.logging.Logger as KtorLogger
 
 internal val client = HttpClient(OkHttp) {
     defaultRequest {
@@ -16,5 +21,13 @@ internal val client = HttpClient(OkHttp) {
     }
     install(ContentNegotiation) {
         json(NetworkConfig.Json)
+    }
+    install(Logging) {
+        logger = object : KtorLogger {
+            override fun log(message: String) {
+                Logger.i(message)
+            }
+        }
+        level = LogLevel.ALL
     }
 }
