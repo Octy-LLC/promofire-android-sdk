acff24111379fe3138e3365c8bda5455f51df314
Konstantin <kostyak13k@gmail.com>
2024-12-09 22:02:01 +0200

    Add Code.isValid extension

--- promofire/src/main/kotlin/io/promofire/models/Code.kt
+++ promofire/src/main/kotlin/io/promofire/models/Code.kt
@@ -1,5 +1,7 @@
 package io.promofire.models
 
+import io.promofire.utils.INFINITY
+import io.promofire.utils.timeInSeconds
 import java.util.Date
 
 public data class Code(
@@ -18,3 +20,21 @@ public data class Code(
         ACTIVE, FULLY_REDEEMED, DEACTIVATED
     }
 }
+
+public val Code.isValid: Boolean
+    get() = isValidByAmount && !isExpired && status == Code.Status.ACTIVE
+
+private val Code.isInfiniteCode: Boolean
+    get() = amount.equals(INFINITY, ignoreCase = true)
+
+private val Code.isExpired: Boolean
+    get() {
+        val currentTimeSeconds = Date().timeInSeconds
+        return currentTimeSeconds >= expiresAt
+    }
+
+private val Code.amountValue: Int?
+    get() = if (isInfiniteCode) null else amount.toIntOrNull()
+
+private val Code.isValidByAmount: Boolean
+    get() = isInfiniteCode || (amountValue ?: 0) > 0
