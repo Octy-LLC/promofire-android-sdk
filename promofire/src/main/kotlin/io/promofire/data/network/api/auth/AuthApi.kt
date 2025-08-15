7cd1809140455669a7760d7bf6aea93a30a99a39
Konstantin <kostyak13k@gmail.com>
2025-08-15 13:48:11 +0300

    Remove SdkAuthRequestDto

--- promofire/src/main/kotlin/io/promofire/data/network/api/auth/AuthApi.kt
+++ promofire/src/main/kotlin/io/promofire/data/network/api/auth/AuthApi.kt
@@ -1,17 +1,12 @@
 package io.promofire.data.network.api.auth
 
 import io.ktor.client.HttpClient
-import io.promofire.data.network.api.auth.requests.SdkAuthRequestDto
 import io.promofire.data.network.api.auth.requests.SdkCustomerAuthRequestDto
 import io.promofire.data.network.api.common.models.AuthResponseDto
 import io.promofire.data.network.core.NetworkResult
 import io.promofire.data.network.core.safePost
 
 internal interface AuthApi {
-
-    suspend fun signInWithSdk(
-        request: SdkAuthRequestDto,
-    ): NetworkResult<AuthResponseDto>
 
     suspend fun signInWithSdk(
         request: SdkCustomerAuthRequestDto,
@@ -21,10 +16,6 @@ internal interface AuthApi {
 internal class AuthApiImpl(
     private val httpClient: HttpClient,
 ) : AuthApi {
-
-    override suspend fun signInWithSdk(
-        request: SdkAuthRequestDto,
-    ): NetworkResult<AuthResponseDto> = httpClient.safePost("auth/sdk", request)
 
     override suspend fun signInWithSdk(
         request: SdkCustomerAuthRequestDto,
